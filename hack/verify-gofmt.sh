3b8374b3bedb54e7a61faac47adadc092a262966
adselvar <adhita94@gmail.com>
2018-08-13 18:35:04 -0700

    changes

--- hack/verify-gofmt.sh
+++ hack/verify-gofmt.sh
@@ -11,7 +11,7 @@ find_files() {
 # formatting (e.g., a file does not parse correctly). Without "|| true" this
 # would have led to no useful error message from gofmt, because the script would
 # have failed before getting to the "echo" in the block below.
-diff=$(find_files | xargs ${gofmt} -d -s 2>&1) || true
+diff=$(find_files | xargs ${gofmt} -I -s 2>&1) || true
 if [[ -n "${diff}" ]]; then
   echo "${diff}"
   exit 1
