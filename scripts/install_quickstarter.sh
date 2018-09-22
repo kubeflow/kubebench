6069c527e102d818964fd31ac1afee423f31cd17
Andrey <31112157+andreyvelich@users.noreply.github.com>
2018-09-22 03:17:23 -0700

    Add namespace to installation script (#113)
    * Added namespace to install_quickstarter.sh
    
    * Fixed getting namespace
    
    * removed space

--- scripts/install_quickstarter.sh
+++ scripts/install_quickstarter.sh
@@ -2,6 +2,7 @@
 
 KB_VERSION=${KB_VERSION:-master}
 KB_ENV=${KB_ENV:-default}
+K8S_NAMESPACE=`ks env describe ${KB_ENV} | grep namespace | awk '{print $2}'`
 
 ks registry add kubebench github.com/kubeflow/kubebench/tree/${KB_VERSION}/kubebench
 ks pkg install kubebench/kubebench-quickstarter@${KB_VERSION}
@@ -12,6 +13,6 @@ ks generate kubebench-quickstarter-volume kubebench-quickstarter-volume
 
 ks apply ${KB_ENV} -c kubebench-quickstarter-service
 
-KB_NFS_IP=`kubectl get svc kubebench-nfs-svc -o=jsonpath={.spec.clusterIP}`
+KB_NFS_IP=`kubectl get svc kubebench-nfs-svc -o=jsonpath={.spec.clusterIP} -n ${K8S_NAMESPACE}`
 ks param set kubebench-quickstarter-volume nfsServiceIP ${KB_NFS_IP}
 ks apply ${KB_ENV} -c kubebench-quickstarter-volume
