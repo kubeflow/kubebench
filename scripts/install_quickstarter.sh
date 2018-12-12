b0fba43289e5d1424ff7116d9fa6f8c2ebc9d39a
Jin Chi He <hejinchi@cn.ibm.com>
2018-12-12 09:40:54 +0800

    update install script to ensure privileged policy

--- scripts/install_quickstarter.sh
+++ scripts/install_quickstarter.sh
@@ -4,6 +4,12 @@ KB_VERSION=${KB_VERSION:-master}
 KB_ENV=${KB_ENV:-default}
 K8S_NAMESPACE=`ks env describe ${KB_ENV} | grep namespace | awk '{print $2}'`
 
+POLICY_CHECK=`kubectl --as=system:serviceaccount:${K8S_NAMESPACE}:default -n ${K8S_NAMESPACE} auth can-i use podsecuritypolicy/privileged`
+if [ "${POLICY_CHECK}" = "no" ]; then
+    kubectl -n ${K8S_NAMESPACE} create role psp:privileged --verb=use --resource=podsecuritypolicy --resource-name=privileged
+    kubectl -n ${K8S_NAMESPACE} create rolebinding ${KB_ENV}:psp:privileged --role=psp:privileged --serviceaccount=${K8S_NAMESPACE}:default
+fi
+
 ks registry add kubebench github.com/kubeflow/kubebench/tree/${KB_VERSION}/kubebench
 ks pkg install kubebench/kubebench-quickstarter@${KB_VERSION}
 ks pkg install kubebench/kubebench-examples@${KB_VERSION}
