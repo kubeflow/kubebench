c69622855ecc5f063519a98d46329412cc7b2a8c
Xinyuan Huang <huangxy0101@gmail.com>
2019-01-09 01:04:44 -0800

    Fix ks packages in test

--- test/py/kubebench/test/deploy_utils.py
+++ test/py/kubebench/test/deploy_utils.py
@@ -110,8 +110,7 @@ def setup_ks_app(test_dir, src_root_dir, namespace, github_token, api_client):
     util.run(["ks", "registry", "add", r, registries[r]], cwd=app_dir)
 
   # Install required packages
-  packages = ["kubeflow/core", "kubeflow/argo", "kubebench/kubebench-job",
-              "kubebench/nfs-server", "kubebench/nfs-volume", "kubeflow/tf-training"]
+  packages = ["kubeflow/argo", "kubeflow/kubebench", "kubeflow/tf-training"]
   for p in packages:
     util.run(["ks", "pkg", "install", p], cwd=app_dir)
 
