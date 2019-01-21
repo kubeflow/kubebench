5e3f813043a2f740cbb7c7d6b536d03d5a79846e
Jiaxin Shan <seedjeffwan@gmail.com>
2019-01-20 23:30:18 -0800

    Do not gofmt vendors

--- hack/update-gofmt.sh
+++ hack/update-gofmt.sh
@@ -16,13 +16,4 @@
 
 set -euo pipefail
 
-echo "Verifying gofmt"
-
-diff=$(find . -name "*.go" | grep -v "\/vendor\/" | xargs gofmt -s -d 2>&1)
-if [[ -n "${diff}" ]]; then
-  echo "${diff}"
-  echo
-  echo "Please run hack/update-gofmt to fix the issue(s)"
-  exit 1
-fi
-echo "No issue found"
+find . -name "*.go" | grep -v "\/vendor\/" | xargs gofmt -s -w
